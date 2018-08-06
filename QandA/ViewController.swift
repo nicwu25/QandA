62453828dc3259708f68089a858b242d1394823c
nicwu <nicwu@nicwude-macbook-pro.local>
2018-08-06 20:38:03 +0800

    Initial Commit

---
+++ QandA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QandA
+//
+//  Created by nicwu on 2018/8/6.
+//  Copyright © 2018年 nicwu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
